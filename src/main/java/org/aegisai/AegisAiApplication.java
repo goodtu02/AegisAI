fe845bd88a5d4124dbe05865e4b59ff7f832a7bb
goodtu02 <qwe13117@gmail.com>
2025-11-07 01:50:46 +0900

    Fix. AegisAiApplication

--- src/main/java/org/aegisai/AegisAiApplication.java
+++ src/main/java/org/aegisai/AegisAiApplication.java
@@ -5,13 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-<<<<<<< HEAD
-@SpringBootApplication  // exclude 제거
+
 @EnableJpaRepositories("org.aegisai.repository")  // Repository 스캔
 @EntityScan("org.aegisai.entity")                  // Entity 스캔
-=======
 @SpringBootApplication
->>>>>>> cde6c049d1d44d84a0c18b997572cc58d72281ab
 public class AegisAiApplication {
     public static void main(String[] args) {
         SpringApplication.run(AegisAiApplication.class, args);

9d76f3e69fb0a680d543a89fdccbe5def7d22199
goodtu02 <qwe13117@gmail.com>
2025-11-07 07:37:55 +0900

    Apicontroller

--- src/main/java/org/aegisai/controller/ApiController.java
+++ src/main/java/org/aegisai/controller/ApiController.java
@@ -38,12 +38,12 @@ public class ApiController {
         else {
             body = new ResponseDto("VULNERABLE", "취약한 코드입니다.");
         }
-        Thread.sleep(1000); // 1초 대기
+        Thread.sleep(2000); // 1초 대기
         body.setXaiDetectionExplanation(apiService.requestModel3(analysisDto)); //llm(프롬프트 필요)
-        Thread.sleep(1000); // 1초 대기
+        Thread.sleep(2000); // 1초 대기
         analysisDto.setFixedcode(apiService.requestModel2(analysisDto)); //code t5
         body.setXaiFixExplanation(apiService.requestModel3_1(analysisDto)); //llm(프롬프트 필요)
-        Thread.sleep(1000); // 1초 대기
+        Thread.sleep(2000); // 1초 대기
         List<VulnerabilitiesDto> vulnerabilities = apiService.requestModel4(analysisDto); //guide llm
         apiService.entityService(vulnerabilities, analysisDto);
         
